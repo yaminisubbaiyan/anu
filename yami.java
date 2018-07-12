44fb975416bd2244082ff6ca478d1792acc4bd5a
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-12 11:56:45 +0530

    Create yami.java

---
+++ yami.java
@@ -0,0 +1,25 @@
+public class positive {
+
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int a=15;
+				if(a>0){
+					System.out.println("The no is positive");
+				}
+				else if(a<0){
+					System.out.println("The no is negative");
+				}
+				else{
+					System.out.println("The no is zero");
+					
+					
+				}
+
+			}
+
+		}
+
+	
+
+
