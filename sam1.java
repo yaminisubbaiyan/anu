6d5bb7b35110c0f3e3eb6e4d2a8a94db6403e63f
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 14:35:23 +0530

    Create sam1.java

---
+++ sam1.java
@@ -0,0 +1,14 @@
+public class alpha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char c='1';
+if(Character.isAlphabetic(c)){
+	System.out.println("alphatic");
+}
+else{
+	System.out.println("non alphatic");
+}
+	}
+
+}
