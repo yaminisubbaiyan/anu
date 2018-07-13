7b50c078b0a5aaf59784b0a63e6bb17766cc850a
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 17:31:48 +0530

    Create tam3.java

---
+++ tam3.java
@@ -0,0 +1,25 @@
+public class leap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=1900;
+if(n%4==0){
+	if(n%100==0){
+		if(n%400==0){
+			System.out.println("leap year");
+		}
+		else{
+			System.out.println("leap year");
+		}
+	}
+	else{
+		System.out.println("not leap year");
+	}
+	}
+else{
+	System.out.println("not leap year");
+}
+
+	}
+
+}
