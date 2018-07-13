7c667b71e30dd8afd9e0fd606df9d5ccd7fd97c9
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 17:08:19 +0530

    Create sam4.java

---
+++ sam4.java
@@ -0,0 +1,19 @@
+public class largenum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int b=3;
+int c=4;
+int d=5;
+if(b>c&&b>d){
+	System.out.println(b);
+}
+else if(c>b&&c>d){
+	System.out.println(c);
+}
+else{
+	System.out.println(d);
+}
+	}
+
+}
