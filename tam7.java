6844ce295c58c0116641add28e2ea784d8feb4f9
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 18:35:04 +0530

    Create tam7.java

---
+++ tam7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+int a;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+a=in.nextInt();
+int b=(int)(Math.pow(n,a));
+System.out.println(b);
+	}
+
+}
