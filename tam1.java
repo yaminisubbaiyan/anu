a315d5f32ab1119ec0502a99d67105bc064a2150
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 17:18:48 +0530

    Create tam1.java

---
+++ tam1.java
@@ -0,0 +1,15 @@
+
+public class alpha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char c='a';
+if(Character.isAlphabetic(c)){
+	System.out.println("alphatic");
+}
+else{
+	System.out.println("non alphatic");
+}
+	}
+
+}
