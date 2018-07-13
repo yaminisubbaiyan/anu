9aa284da21704e1a929f44cacb7c393af43822cc
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 16:52:32 +0530

    Create sam3.java

---
+++ sam3.java
@@ -0,0 +1,14 @@
+public class hlo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hello";
+int i,n=2;
+String s1[]=new String[n];
+for(i=0;i<n;i++){
+	s1[i]=s;
+	System.out.println(s1[i]);
+}
+	}
+
+}
