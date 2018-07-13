1fea030a39a9020a56712ac5e9de08dcbd75a441
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 18:03:18 +0530

    Create tam4.java

---
+++ tam4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class nature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+int sum=0;
+for(i=1;i<=n;i++){
+sum=sum+i;
+	}
+System.out.println(sum);
+	}
+}
