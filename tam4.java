05cb12900228547a6f0a13a8e0e6ca750446f96f
yaminisubbaiyan <41136738+yaminisubbaiyan@users.noreply.github.com>
2018-07-13 17:38:10 +0530

    Create tam4.java

---
+++ tam4.java
@@ -0,0 +1,12 @@
+public class nature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n=2;
+int sum=0;
+for(i=1;i<=n;i++){
+sum=sum+i;
+	}
+System.out.println(sum);
+	}
+}
